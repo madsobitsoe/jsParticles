96f4636e9e72bd7a71c5110b75769709f9e3d3d1
Mads Obitsø Thomsen <madsobitsoe@gmail.com>
2014-09-23 16:38:44 +0200

    trying out gh-pages

--- jsParticles.js
+++ jsParticles.js
@@ -16,16 +16,17 @@ canvas.setAttribute('tabindex', 1);
 var ctx = canvas.getContext("2d");
 var FPS = 1000 / 60;
 // Create gravity vector
-var gravity = { angle:pi, length:1.75 };
+var gravity = { angle:pi, length: 0.1 };
 
 // Create counter for for creating new particles
 var counter = 0;
 // Create array to hold particles
-var ParticleArray = [];
+var particleArray = [];
+var springArray = [];
 
 var dist = 999999, overlap = 0;
 //Create some booleans for html interaction
-var useGravity = true, useDrag = true, useCollisions = true;
+var useGravity = true, useDrag = true, useCollisions = true, spawnPartSys = true;
 
 // Request animation frame from multiple browser engines
 window.requestAnimFrame = (function(){ 
@@ -46,10 +47,6 @@ window.cancelRequestAnimFrame = (function() {
 	|| clearTimeout }
                                 )();
 
-
-
-
-
 // Particle class
 // -----------------------------------------------------------------------------
 function Particle(X, Y, vector, radius, mass)
@@ -67,33 +64,34 @@ function Particle(X, Y, vector, radius, mass)
         {
             this.X = 2 * (Width - this.radius) - this.X;
             this.vector.angle *= -1;
-            this.vector.length *= this.Elasticity;
+            this.vector.length *= this.elasticity;
         }
         if (this.X < this.radius)
         {
             this.X = 2 * this.radius - this.X;
             this.vector.angle *= -1;
-            this.vector.length *= this.Elasticity;
+            this.vector.length *= this.elasticity;
         }
 
         if(this.Y > Height - this.radius)
         {
             this.Y = 2 * (Height - this.radius) - this.Y;
             this.vector.angle = - this.vector.angle;
-            this.vector.length = (this.vector.length * this.Elasticity) * -1 ;
+            this.vector.length = (this.vector.length * this.elasticity) * -1 ;
         }
 
         if(this.Y  < this.radius)
         {
             this.Y = 2 * this.radius - this.Y;
-            this.vector.angle = - this.vector.angle;
-            this.vector.length = (this.vector.length * this.Elasticity) * -1 ;
+            this.vector.angle *= -1;
+            //            this.vector.angle -= pi;
+            this.vector.length = (this.vector.length * this.elasticity) * -1 ;
         }
     };
 
+    // Update position based on speed and angle 
     this.Move = function()
     {
-        // Update position based on speed and angle 
         this.X += Math.sin(this.vector.angle) * this.vector.length;
         this.Y -= Math.cos(this.vector.angle) * this.vector.length;
     };
@@ -103,6 +101,13 @@ function Particle(X, Y, vector, radius, mass)
 	this.X = 200;
 	this.Y = 200;
     };
+
+    // Change angle and speed by adding a vector to movement vector
+    this.Accelerate = function(vector)
+    {
+        this.vector = AddVectors(this.vector, vector);
+    };
+
     this.ExperienceDrag = function()
     {
         this.vector.length *= this.Drag;   
@@ -134,11 +139,11 @@ Particle.prototype =
     {
         X: 50,
         Y: 50,
-        radius: 10,
-        vector: { angle:(pi/4), length: 10 },
+        radius: 20,
+        vector: { angle:pi, length: 0 },
         mass: 10,
-        Drag: 0.997,
-        Elasticity: 0.75,
+        Drag: 0.99,
+        elasticity: 1,
         Color: '#911',
         Paint: function()
         {
@@ -149,6 +154,78 @@ Particle.prototype =
         }
     };
 Particle.prototype.constructor = Particle;
+
+
+
+
+
+
+// Spring class
+// :--------------------------------------------------------------------------:
+function Spring(p1, p2, length)
+{
+    // The particles the spring stretches between
+    this.p1 = p1;
+    this.p2 = p2;
+
+
+    // The "natural" length of the spring
+    this.length = length || Spring.prototype.length;
+    // The strength of the spring                                       
+    //    this.strength = strength || Spring.prototype.strength;
+
+    //Spring constant. Is negative in calculations
+    // Is "fighting" the suppression of the spring
+    //    this.k = k || Spring.prototype.k;
+
+    // Damping constant
+    //  this.b = b  || Spring.prototype.b;
+    // The natural frequency of the spring 
+    // this.freq = (0.5 * pi) * Math.sqrt(k / (p1.mass + p2.mass));
+    
+
+    this.Paint = function()
+    {
+        ctx.beginPath();
+        ctx.moveTo(p1.X, p1.Y);
+        ctx.lineTo(p2.X, p2.Y);
+        ctx.stroke();
+
+    };
+
+    this.Update = function()
+    {
+
+        var dx = this.p1.X - this.p2.X;
+        var dy = this.p1.Y - this.p2.Y;
+        var dist = Math.sqrt(dx*dx + dy*dy);
+        
+        var theta = Math.atan2(dy, dx);
+        var force = (this.length - dist) * this.strength;
+        
+        this.p1.Accelerate({ angle:theta, length:(force/this.p1.mass ) }); 
+        this.p2.Accelerate({ angle:theta, length:(force/this.p2.mass ) });
+
+    };
+}
+
+
+
+Spring.prototype =
+    {
+        length: 100,
+        strength: 0.01
+
+    }; 
+
+Spring.prototype.constructor = Spring;
+
+function AddSpring(p1, p2, length)
+{
+    
+    springArray.push(new Spring(p1, p2, length));
+
+}
 
 // Create a background object
 var BG = {
@@ -165,16 +242,37 @@ function Paint(){
     BG.Paint();
 
     //Paint all the particles
-    for (i = 0; i < ParticleArray.length; i++)
+    for (i = 0; i < particleArray.length; i++)
     {
-        ParticleArray[i].Paint();
+        particleArray[i].Paint();
+    }
+    
+    // Paint all the springs
+    for (i = 0; i < springArray.length; i++)
+    {
+        springArray[i].Paint();
+        
     }
 }
-
 function NewGame()
 {
+    toggleReset();
+    // var p1 = new Particle(100, 50, { angle:pi, length: 1}, 10, 100);
+    // var p2 = new Particle(150, 150, { angle:pi, length: 1}, 10, 100);
+    // var p3 = new Particle(50, 150, { angle:pi, length: 1}, 10, 100);
+//    var p4 = new Particle(50, 150);
 
-    
+    // particleArray.push(p1);
+    // particleArray.push(p2);
+    // particleArray.push(p3);    
+  //  particleArray.push(p4);
+//    AddSpring(p1, p2, 100);
+  //  AddSpring(p2, p3, 100);
+//    AddSpring(p3, p4, 100);
+    // AddSpring(p4, p1, 100);
+ //  AddSpring(p3, p1, 100);
+    // AddSpring(p2, p4, 100);
+
     Loop();
 }
 
@@ -183,34 +281,45 @@ function Loop()
     init = requestAnimFrame(Loop);
     Paint();
     
-    for (i = 0; i < ParticleArray.length; i++)
+    // Update Particles
+    for (i = 0; i < particleArray.length; i++)
     {
-        ParticleArray[i].Update();
+        particleArray[i].Update();
         if (useCollisions)
         {
-            for (j = i + 1; j < ParticleArray.length; j++)
+            for (j = i + 1; j < particleArray.length; j++)
             {
-                Collide(ParticleArray[i], ParticleArray[j]);
+                Collide(particleArray[i], particleArray[j]);
             }
         }       
+        
     }
-    if (6 / counter == 1)
-    {
-        ParticleArray.push(new Particle(Width/2, 100, ({ angle:(pi/4), length:10 }), (ParticleArray.length + 1) / 2, ParticleArray.length));
-        counter = 0;
-    }
-        counter++;
 
-    if (ParticleArray.length > 40)
+    // Update Springs
+    for (i = 0; i < springArray.length; i++)
     {
-        ParticleArray = [];
+        springArray[i].Update();
+    }
+
+    if (spawnPartSys)
+    {
+        if (particleArray.length < 10)
+        {
+
+            if (60 / counter == 1)
+            {
+                particleArray.push(new Particle(Width/2, 100, { angle:(pi/4), length:1 }, 10, 100));
+                counter = 0;
+            }
+        }
+
+        counter++;
 
     }
 };
 
-// Start the main loop
+// the main loop Game
 NewGame();
-
 
 
 
@@ -253,8 +362,8 @@ function Collide(p1, p2)
             
         );
 
-        p1.vector.length *= p1.Elasticity;
-        p2.vector.length *= p2.Elasticity;
+        p1.vector.length *= p1.elasticity;
+        p2.vector.length *= p2.elasticity;
 
 
 
@@ -284,6 +393,8 @@ function AddVectors (vector1, vector2)
 
 
 
+
+
 // Functions to toggle settings from html buttons
 function toggleGravity()
 {
@@ -297,4 +408,15 @@ function toggleDrag()
 function toggleCollisions()
 {
     useCollisions = !useCollisions;
+}
+
+function toggleReset()
+{
+    particleArray = [];
+}
+
+function toggleSpawnPartSys()
+{
+    spawnPartSys = !spawnPartSys;
+    particleArray = [];
 }
